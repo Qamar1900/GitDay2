87184289140d5fdd5b5048891a96970c66506171
qamar <qamar@HPComputer.mynetworksettings.com>
2022-08-01 19:21:46 -0400

    implemented  US123 , US345

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -4,4 +4,12 @@ public class DemoTest {
 public static void main(String[] args) {
 	System.out.println("Test...");
 }
+//for US123.
+public static void add(int a, int b) {
+	System.out.println("a+b");
+}
+//for US345
+public static void divide(int a, int b) {
+	System.out.println(a/b);
+}
 }
